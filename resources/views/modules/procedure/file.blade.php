e2aa18fc5b849a3c7eac5959857f37b060be63d7
Ower A. Campos <owerion22@gmail.com>
2021-10-25 11:50:42 -0500

    se realiza cambio en la vista de archivos en procedimientos

--- resources/views/modules/procedure/file.blade.php
+++ resources/views/modules/procedure/file.blade.php
@@ -3,24 +3,26 @@
 @section('space')
 <div class="container-fluid">
   <h6>ARCHIVO DE PROCEDIMIENTOS</h6>
-  <table id="tableDatatable" class="table table-borderless table-striped text-center w-100">
+  <table class="w-100 table table-striped text-center" id="tableDatatable">
     <thead>
-      <th>N°</th>
-      <th>DOCUMENTO</th>
-      <th>CONTENIDO</th>
-      <th>ACCIONES</th>
+      <tr>
+        <th>N°</th>
+        <th>DOCUMENTO</th>
+        <th>CONTENIDO</th>
+        <th>ACCIONES</th>
+      </tr>
     </thead>
     <tbody>
       @php $row = 1; @endphp
       @foreach($allfiles as $file)
+      @if($file->pro_status == "approved")
       <tr>
-        @if($file->pro_status == "approved")
         <td class="align-middle">{{ $row++ }}</td>
         <td class="align-middle">{{ $file->domName}}</td>
         <td class="align-middle">{{ substr($file->pro_content,0,80)."..."}}</td>
-        <td><button class="btn btn-outline-danger btn-PDF"><i class="far fa-file-pdf"></i><span hidden>{{$file->pro_id}}</span></button></td>
-        @endif
+        <td><button class="btn btn-outline-danger btn-PDF" title="PDF"><i class="far fa-file-pdf"></i><span hidden>{{$file->pro_id}}</span></button></td>
       </tr>
+      @endif
       @endforeach
     </tbody>
   </table>
