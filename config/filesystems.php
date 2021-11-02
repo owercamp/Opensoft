2703044bd4b4f73e4bcdb007cbf6bd271acce301
Ower A. Campos <owerion22@gmail.com>
2021-11-02 11:47:40 -0500

    instalación browserSync
    se instala browserSync y se ajusta ruta storage.

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,7 +57,7 @@ return [
 
     'opensoft' => [
       'driver' => 'local',
-      'root' => dirname('public'),
+      'root' => dirname('./storage'),
       'url' => env('APP_URL') . '/storage',
       'visibility' => 'public',
     ],
